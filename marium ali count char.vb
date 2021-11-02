d1ddfd452497886d4a62ec1442e376e60514ec40
MariumAl <93540809+MariumAl@users.noreply.github.com>
2021-11-02 19:35:18 +0500

    Add files via upload

---
+++ marium ali count char.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1 As String
+        Dim count, x As Integer
+        Dim char1, thischar As Char
+        str1 = ""
+        count = 0
+        char1 = ""
+        x = 0
+        Console.Write("enter string text: ")
+        str1 = Console.ReadLine()
+        Console.Write("enter character:")
+        char1 = Console.ReadLine()
+        For x = 1 To Len(str1)
+            thischar = Mid(str1, x, 1)
+            If thischar = char1 Then
+                count = count + 1
+            End If
+        Next
+        Console.Write("Number of times character appeared: " & count)
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ marium ali delete char.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, Str2 As String
+        Dim thisChar, Char1 As Char
+        Dim Count As Integer
+        Str1 = ""
+        Str2 = ""
+        thisChar = ""
+        Count = 0
+        Console.Write("Enter text: ")
+        Str1 = Console.ReadLine
+        Console.Write("Enter character to delete:")
+        Char1 = Console.ReadLine
+        For Count = 1 To Len(Str1)
+            thisChar = Mid(Str1, Count, 1)
+            If thisChar <> Char1 Then
+                Str2 = Str2 & thisChar
+            End If
+        Next
+        Console.WriteLine("processed string:" & Str2)
+        Console.ReadLine()
+    End Sub
+
+End Module
